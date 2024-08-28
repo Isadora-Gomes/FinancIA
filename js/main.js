308f2d7bace9b4312a266b936e904dc06fdb90b5
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2024-08-28 17:41:56 -0300

    sweetalert inicio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 // área orçamento
 function alerta(){
     Swal.fire({
-        title: "Qual é o valor que deseja adcionar?",
-        showDenyButton: true,
-        confirmButtonText: "Entrada",
-        denyButtonText: `Saida`
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire("Saved!", "", "success");
-        } else if (result.isDenied) {
-          Swal.fire("Saved!", "", "success");
-        }
-    });
+        title: 'Registro de Transação',
+        html: `
+            <label for="tipoTransacao">Tipo de Transação:</label>
+            <select id="tipoTransacao" class="swal2-input">
+                <option value="entrada">Entrada</option>
+                <option value="saida">Saída</option>
+            </select>
+            <label for="valorTransacao">Valor:</label>
+            <input type="number" id="valorTransacao" class="swal2-input" placeholder="Digite o valor">
+            <label for="dataTransacao">Data:</label>
+            <input type="date" id="dataTransacao" class="swal2-input">
+        `,
+    })
 }
 // fim área orçamento
