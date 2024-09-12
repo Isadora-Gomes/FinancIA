cc3f6b1290d5b1ede5d85b0a02d64eb6b45652e6
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2024-09-12 14:42:08 -0300

    js orçamento

--- js/main.js
+++ js/main.js
@@ -1,55 +1,79 @@
 // área orçamento
-function alerta(){
-    Swal.fire({
-        title: 'Informe os dados:',
-        html: `
-          <label for="tipo">Tipo:</label>
-          <select id="tipo">
-            <option value="entrada">Entrada</option>
-            <option value="saida">Saída</option>
-          </select><br><br>
-          
-          <label for="valor">Valor:</label>
-          <input type="number" id="valor"><br><br>
-          
-          <label for="data">Data:</label>
-          <input type="date" id="data">
-        `,
-        showCancelButton: true,
-        confirmButtonText: 'Confirmar',
-        cancelButtonText: 'Cancelar',
-        preConfirm: () => {
-          const tipo = Swal.getPopup().querySelector('#tipo').value;
-          const valor = Swal.getPopup().querySelector('#valor').value;
-          const data = Swal.getPopup().querySelector('#data').value;
+function alerta() {
+  Swal.fire({
+      title: 'Informe os dados:',
+      html: `
+        <label for="tipo">Tipo:</label>
+        <select id="tipo">
+          <option value="entrada">Entrada</option>
+          <option value="saida">Saída</option>
+        </select><br><br>
+        
+        <label for="valor">Valor:</label>
+        <input type="number" id="valor"><br><br>
+        
+        <label for="data">Data:</label>
+        <input type="date" id="data">
+      `,
+      showCancelButton: true,
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar',
+      preConfirm: () => {
+        const tipo = Swal.getPopup().querySelector('#tipo').value;
+        const valor = Swal.getPopup().querySelector('#valor').value;
+        const data = Swal.getPopup().querySelector('#data').value;
 
-          if (!valor || !data) {
-            Swal.showValidationMessage('Por favor, preencha todos os campos!');
+        if (!valor || !data) {
+          Swal.showValidationMessage('Por favor, preencha todos os campos!');
+        }
+
+        return { tipo, valor, data };
+      }
+  }).then((result) => {
+      if (result.isConfirmed) {
+          const tipoObtido = result.value.tipo; 
+          const dataObtida = result.value.data; 
+          let valorObtido = parseFloat(result.value.valor);
+          //area total
+          const valorInicialElement = document.querySelector('#idTotal h5');
+          let valorInicialTotal = parseFloat(valorInicialElement.innerHTML.replace('R$', '').replace(',', '.').trim());
+          let valorFinalTotal = valorInicialTotal;
+
+          if (tipoObtido === "entrada") {
+              valorFinalTotal += valorObtido;
+              //area de entradas 
+              const valorInicialEntradaElement = document.querySelector('#idEntradas h5');
+              let valorInicialEntrada = parseFloat(valorInicialEntradaElement.innerHTML.replace('R$', '').replace(',', '.').trim());
+              let valorFinalEntrada = valorInicialEntrada;
+
+              valorFinalEntrada += valorObtido;
+
+              valorInicialEntradaElement.innerHTML = `R$ ${valorFinalEntrada.toFixed(2)}`;
+
+          } else if (tipoObtido === "saida") {
+              valorFinalTotal -= valorObtido;
+              //area saidas
+              const valorInicialSaidaElement = document.querySelector('#idSaidas h5');
+              let valorInicialSaida = parseFloat(valorInicialSaidaElement.innerHTML.replace('R$', '').replace(',', '.').trim());
+              let valorFinalSaida = valorInicialSaida;
+
+              valorFinalSaida += valorObtido;
+
+              valorInicialSaidaElement.innerHTML = `R$ ${valorFinalSaida.toFixed(2)}`;
           }
-  
-          return { tipo, valor, data };
-        }
-      }).then((result) => {
-        if (result.isConfirmed) {
-            tipoObtido = result.value.tipo; 
-            dataObtida = result.value.data; 
-            valorObtido = result.value.valor; 
-            valorInicial = document.getElementsByClassName(".orcamento h5")
-            var x = valorObtido + valorInicial
-            document.querySelector('#transacoes tbody').innerHTML += `
-                <tr>
-                    <td>${tipoObtido}</td>
-                    <td>R$ ${valorObtido}</td>
-                    <td>${dataObtida}</td>
-                </tr>
-            `;
-            document.querySelector('.orcamento h5').innerHTML = `
-                R$ ${x}
-            `;
-        }
-    });
+
+          document.querySelector('#transacoes tbody').innerHTML += `
+              <tr>
+                  <td>${tipoObtido}</td>
+                  <td>R$ ${valorObtido.toFixed(2)}</td>
+                  <td>${dataObtida}</td>
+              </tr>
+          `;
+
+          
+          valorInicialElement.innerHTML = `R$ ${valorFinalTotal.toFixed(2)}`; 
+          
+      }
+  });
 }
 // fim area orçamento
-// area financiamento
-
-
